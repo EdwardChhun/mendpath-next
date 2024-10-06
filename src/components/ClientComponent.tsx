f5c93f96e34b2ca9502640e1e4ffe686aa10aa67
Edward <edwardchhun3@gmail.com>
2024-10-05 22:59:05 -0400

    first init

---
+++ src/components/ClientComponent.tsx
@@ -0,0 +1,18 @@
+// ./components/ClientComponent.tsx
+"use client";
+import { VoiceProvider } from "@humeai/voice-react";
+import Messages from "./Controls";
+import Controls from "./Messages";
+
+export default function ClientComponent({
+  accessToken,
+}: {
+  accessToken: string;
+}) {
+  return (
+    <VoiceProvider auth={{ type: "accessToken", value: accessToken }}>
+      <Messages />
+      <Controls />
+    </VoiceProvider>
+  );
+}
